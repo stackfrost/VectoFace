48d129a55f5a5a92c913d0e682aa89e428614d2a
stackfrost <qwertypoiu461@gmail.com>
2026-08-06 23:45:34 +0530

    Configure Prisma 7 Pg adapter and deployment fixes

--- .next/dev/server/chunks/ssr/_next-internal_server_app_terms_page_actions_0h-0lt4.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/terms/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_terms_page_actions_0h-0lt4.js.map
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const connectionString = process.env.DATABASE_URL;
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient();
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const db = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
-
-export default db;
